6d3832a476251ee7ac30028d3017a6db4bfc5fcf
Zeeshan Ramzan <114761576+ZeeshanRamzan06@users.noreply.github.com>
2024-05-13 13:08:57 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+var typed = new Typed('#typed', {
+    strings: ['educating voters','conducting surveys','making polls', 'running contests','selecting leaders ', 'making decisions', 'team collaboration'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+});
